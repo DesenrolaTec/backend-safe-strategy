e57e8a89126b235a001192cb289a7e37aede9b87
Guilherme <guilhermeanacleto@hotmail.com>
2024-09-21 21:29:49 -0300

    Ajusta regra de negocio user

--- app/application/exceptions/user_exceptions.py
+++ app/application/exceptions/user_exceptions.py
@@ -1,1 +1,5 @@
 # User exceptions 
+
+class InvalidBirthdayError(Exception):
+    """Exceção para datas de aniversário inválidas."""
+    pass
--- app/domain/classes/user.py
+++ app/domain/classes/user.py
@@ -1,59 +1,25 @@
+import re
 from datetime import datetime
+from application.exceptions.user_exceptions import InvalidBirthdayError
 
 class User():
-    def __is_valid_email(self, email: str):
-        """
-            Validates the given email address.
-
-            This method checks if the email conforms to standard email formatting rules.
-            It uses a regular expression to ensure the email:
-            - Contains a valid username part (alphanumeric characters, underscores, dots, percentages, pluses, and hyphens).
-            - Includes an '@' symbol followed by a valid domain name.
-            - Ends with a top-level domain (TLD) that consists of at least two alphabetic characters.
-            
-            Additionally, it ensures that the email length does not exceed 254 characters.
-
-            Args:
-                email (str): The email address to validate.
-
-            Returns:
-                str: The validated email address if it is valid.
-
-            Raises:
-                ValueError: If the email is invalid or exceeds the length limit.
-        """
+    def __ensure_valid_email(self, email: str)->str:
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         if len(email) > 254:
-            raise ValueError("Email muito longo")
+            raise ValueError("Email is too long")
         if re.match(pattern, email):
             return email
         else:
-            raise ValueError("Email inválido")
+            raise ValueError("Email is not valid")
 
-    def __is_cpf_valid(self, cpf: str) -> bool:
-        """
-            Validates the given CPF (Cadastro de Pessoas Físicas) number.
-
-            This method checks if the CPF is valid according to Brazilian CPF validation rules.
-            It verifies that the CPF consists of exactly 11 digits, and performs a series of
-            calculations to ensure that the check digits are correct.
-
-            Args:
-                cpf (str): The CPF number to validate.
-
-            Returns:
-                bool: True if the CPF is valid, False otherwise.
-
-            Raises:
-                ValueError: If the CPF contains non-numeric characters or is not exactly 11 digits.
-        """
+    def __ensure_valid_cpf(self, cpf: str) -> str:
         cpf = cpf.replace(".", "").replace("-", "")
         
         if not (cpf.isdigit() and len(cpf) == 11):
-            return False
+            raise ValueError(f"Invalid CPF: {cpf}")
 
         if cpf == cpf[0] * 11:
-            return False
+            raise ValueError(f"Invalid CPF: {cpf}")
 
         def calcular_digito(cpf_parcial: str) -> int:
             peso = len(cpf_parcial) + 1
@@ -63,34 +29,15 @@ class User():
         
         primeiro_digito = calcular_digito(cpf[:9])
         if primeiro_digito != int(cpf[9]):
-            return False
+            raise ValueError(f"Invalid CPF: {cpf}")
         
         segundo_digito = calcular_digito(cpf[:10])
         if segundo_digito != int(cpf[10]):
-            return False
+            raise ValueError(f"Invalid CPF: {cpf}")
         
-        return True
+        return cpf
     
-    def __is_password_secure(self, password: str):
-        """
-        Validates the security of the given password.
-
-        This method checks if the password meets specific security criteria:
-        - Minimum length of 8 characters.
-        - At least one uppercase letter.
-        - At least one lowercase letter.
-        - At least one digit.
-        - At least one special character (e.g., !@#$%^&*).
-
-        Args:
-            password (str): The password to validate.
-
-        Returns:
-            bool: True if the password is secure, False otherwise.
-
-        Raises:
-            ValueError: If the password does not meet the security requirements.
-        """
+    def __ensure_password_secure(self, password: str):
         if len(password) < 8:
             raise ValueError("Password must be at least 8 characters long.")
         if not re.search(r'[A-Z]', password):
@@ -102,14 +49,20 @@ class User():
         if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
             raise ValueError("Password must contain at least one special character.")
 
-        return True
+        return password
+
+    def __ensure_valid_birthday(self, birthday: str):
+        birthday = datetime.strptime(birthday, '%Y-%m-%d')
+        today = datetime.today()
+        age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+        if age < 18:
+            raise InvalidBirthdayError("Person must be at least 18 years old.")
+        return birthday.strftime('%Y-%m-%d')
 
     def __init__(self, name: str, email: str, password: str, cpf: str, birthday: datetime)->None:
         self.__name = name
-        if not self.__is_valid_email(email):
-            raise ValueError(f"Invalid Email: {email}")
-        self.__email = email
-        if not self.__is_cpf_valid(cpf):
-            raise ValueError(f"Invalid CPF: {cpf}")
-        self.cpf = cpf
-        if not self.__is_password_secure(password):
+        self.__email = self.__ensure_valid_email(email=email)
+        self.__password = self.__ensure_password_secure(password=password)
+        self.__cpf = self.__ensure_valid_cpf(cpf=cpf)
+        self.__birthday = self.__ensure_valid_birthday(birthday=birthday)
+        
