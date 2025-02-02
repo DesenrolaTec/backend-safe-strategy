bb52c6b563f3d6851d09f456998ac471af49d8d4
Guilherme Anacleto Ferreira <guilhermeanacleto@hotmail.com>
2025-02-01 22:13:52 -0300

    ajusta primary key

--- app/src/infra/models/groups_has_users_model.py
+++ app/src/infra/models/groups_has_users_model.py
@@ -3,7 +3,7 @@ from app.src.infra.adapters.sql_alchemy_adapter import db
 
 class GroupsHasUsersModel(db.Model):
     __tablename__ = 'groups_has_users'
+    users_id= Column(Integer, primary_key=True, nullable=True)
     groups_id = Column(Integer, nullable=True)
-    users_id= Column(Integer, nullable=True)
     created_at = Column(String, nullable=True, default=None)
     updated_at = Column(String, nullable=True, default=None)
