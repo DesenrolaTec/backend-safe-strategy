c7ef45391eb7b4ce8500d1a1993a10fd4f36b39e
Guilherme Anacleto Ferreira <guilhermeanacleto@hotmail.com>
2025-03-05 23:00:04 -0300

    a

--- app/src/infra/entrypoints/bootstrap/bootstrap.py
+++ app/src/infra/entrypoints/bootstrap/bootstrap.py
@@ -6,7 +6,7 @@ from app.src.application.controllers.connection_controller import ConnectionCont
 from app.src.application.controllers.groups_controller import GroupsController
 from app.src.application.repositories.activations.activations_repository import ActivationsRepository
 from app.src.application.repositories.activations_has_groups.activations_has_groups_repository import \
-    ActivationsHasGroupsRepositoryInterface
+    ActivationsHasGroupsRepositoryInterface, ActivationsHasGroupsRepository
 from app.src.application.repositories.strategies.strategies_repository import StrategiesRepository
 from app.src.application.usecases.activations.read_activations_usecase import ReadActivationsUsecase
 from app.src.application.usecases.connections.delete_connections_usecase import DeleteConnectionsUsecase
@@ -51,7 +51,7 @@ class Bootstrap:
         groups_has_users = GroupsHasUsersRepository(session=session)
         strategies_repository = StrategiesRepository(session=session)
         activations_repository = ActivationsRepository(session=session)
-        activations_has_groups = ActivationsHasGroupsRepositoryInterface(session=session)
+        activations_has_groups = ActivationsHasGroupsRepository(session=session)
 
         create_user = CreateUserUsecase(user_repository, profile_repository=conn_repository)
         read_user = ReadUserUsecase(user_repository, conn_repository, organization_repository=org_repo)
